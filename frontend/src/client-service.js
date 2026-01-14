7fdd9b2eae55a5c25236d87e1267346951d1b276
Austin Jerich <austinjrch8@gmail.com>
2026-01-14 13:20:59 -0600

    remove more console.log

--- frontend/src/client-service.js
+++ frontend/src/client-service.js
@@ -1,7 +1,6 @@
 const API_BASE = (process.env.REACT_APP_API_URL || 'http://localhost:3001').replace(/\/$/, '')
 
 async function request(path, opts) {
-  console.log("API_BASE + PATH: ",`${API_BASE}${path}`)
   const res = await fetch(`${API_BASE}${path}`, opts)
   if (!res.ok) {
     const text = await res.text().catch(() => '')
